3a0551b1bff6386e326e3c290b97d9493ed934ca
eduproassist <eduproassist44@gmail.com>
2025-11-21 18:24:39 +0200

    Create firebaseauth.js

---
+++ firebaseauth.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAAWp9JYQY8YThZEW-vvZ40ffIfS8w7H4w",
+    authDomain: "eduproassistprj.firebaseapp.com",
+    projectId: "eduproassistprj",
+    storageBucket: "eduproassistprj.firebasestorage.app",
+    messagingSenderId: "984975777882",
+    appId: "1:984975777882:web:aab9f211d4b7757421f863"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
